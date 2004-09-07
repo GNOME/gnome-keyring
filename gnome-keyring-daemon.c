89907a6ef1ce3f6f7d57a7121fd8e06e5373c2e5
Alexander Larsson <alexl@redhat.com>
2004-09-07 07:08:05 +0000

    Include string.h to fix build (#151825)
    2004-09-07  Alexander Larsson  <alexl@redhat.com>
    
    	* gnome-keyring.c:
    	* gnome-keyring-daemon.c:
    	Include string.h to fix build (#151825)

--- gnome-keyring-daemon.c
+++ gnome-keyring-daemon.c
@@ -27,6 +27,7 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/socket.h>
--- gnome-keyring.c
+++ gnome-keyring.c
@@ -33,6 +33,7 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
