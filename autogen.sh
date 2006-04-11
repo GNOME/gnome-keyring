8b5a4a9b0382d8b6f5c795836f28c505b1231e1a
Sven Herzberg <herzi@gnome-de.org>
2006-04-11 12:01:26 +0000

    accept command line parameters satisfy stupid make dependency
    2006-04-11  Sven Herzberg  <herzi@gnome-de.org>
    
            * autogen.sh: accept command line parameters
    	* reference/tmpl/empty.sgml: satisfy stupid make dependency

--- autogen.sh
+++ autogen.sh
@@ -19,4 +19,4 @@ which gnome-autogen.sh || {
     echo "You need to install gnome-common from the GNOME CVS"
     exit 1
 }
-USE_GNOME2_MACROS=1 . gnome-autogen.sh --enable-gtk-doc
+USE_GNOME2_MACROS=1 . gnome-autogen.sh --enable-gtk-doc $@
