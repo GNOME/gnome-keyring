7f9f98ebeadff4bc3320f2400335f458a0599ede
Stefan Walter <stefw@src.gnome.org>
2007-06-14 21:00:39 +0000

    Include missing license and attribution.
    svn path=/trunk/; revision=628

--- library/gnome-keyring-memory.c
+++ library/gnome-keyring-memory.c
@@ -60,6 +60,30 @@ static GStaticMutex memory_mutex = G_STATIC_MUTEX_INIT;
  * BLOCK SUBALLOCATION
  */
 
+/* suba - sub-allocate memory from larger chunk of memory
+ * Copyright (c) 2003 Michael B. Allen <mba2000 ioplex.com>
+ *
+ * The MIT License
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 typedef size_t ref_t;  /* suba offset from start of memory to object */
 
 #define SUBA_MAGIC "\xFF\x15\x15\x15SUBA"
