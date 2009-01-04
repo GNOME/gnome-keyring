d770ef2c02f258a9be8cb15128b6bc9242558c12
Stefan Walter <stefw@src.gnome.org>
2009-01-04 21:38:10 +0000

    Move old PKCS#11 code out of the way (but still functiontal)
    	* daemon/pkcs11/gkr-pkcs11-dispatch.c: (moved from gkr-pkcs11-daemon.c)
    	* daemon/pkcs11/gkr-pkcs11-dispatch.h: (moved from gkr-pkcs11-daemon.h)
    	* daemon/pkcs11/gkr-pkcs11-dispatch-session.c: (moved from gkr-pkcs11-daemon-session.c)
    	* daemon/pkcs11/Makefile.am: Move old PKCS#11 code out of the way (but still functiontal)
    
    svn path=/trunk/; revision=1423

---
+++ daemon/pkcs11/gkr-pkcs11-dispatch.h
@@ -0,0 +1,39 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 8; tab-width: 8 -*- */
+/* gkr-pkcs11-dispatch.h - entry points for daemon PKCS#11 functionality
+
+   Copyright (C) 2007, Nate Nielsen
+
+   The Gnome Keyring Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   The Gnome Keyring Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with the Gnome Library; see the file COPYING.LIB.  If not,
+   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+
+   Author: Nate Nielsen <nielsen@memberwebs.com>
+*/
+
+#ifndef __GKR_PKCS11_DISPATCH_H__
+#define __GKR_PKCS11_DISPATCH_H__
+
+/* -----------------------------------------------------------------------------
+ * GENERAL FUNCTIONS 
+ */
+ 
+gboolean        gkr_pkcs11_dispatch_setup               (void);
+
+/* -----------------------------------------------------------------------------
+ * Used by gkr-pkcs11-dispatch.c and gkr-pkcs11-dispatch-session.c 
+ */
+
+gpointer        gkr_pkcs11_dispatch_session_thread      (gpointer user_data);
+
+#endif /* __GKR_PKCS11_DISPATCH_H__ */
