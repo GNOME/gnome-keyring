dea4a4907498b196f499e809242ff03407b38b51
Stef Walter <stef@memberwebs.com>
2009-12-23 16:35:09 +0000

    [egg] A few more parsing tests.

--- egg/tests/test-asn1x.c
+++ egg/tests/test-asn1x.c
@@ -8,6 +8,7 @@
 
 #define extern
 #include "egg/asn1-def-pkix.h"
+#include "egg/asn1-def-pk.h"
 #undef extern
 
 static int
@@ -16,6 +17,30 @@ run (void)
 	GNode *asn;
 	gpointer data;
 	gsize n_data;
+
+	data = testing_data_read ("test-pkcs7-1.der", &n_data);
+	asn = egg_asn1x_create (pkix_asn1_tab, "pkcs-7-ContentInfo");
+	egg_asn1x_dump (asn);
+	if (!egg_asn1x_decode (asn, data, n_data))
+		g_assert_not_reached ();
+	egg_asn1x_destroy (asn);
+	g_free (data);
+
+	data = testing_data_read ("test-rsakey-1.der", &n_data);
+	asn = egg_asn1x_create (pk_asn1_tab, "RSAPrivateKey");
+	egg_asn1x_dump (asn);
+	if (!egg_asn1x_decode (asn, data, n_data))
+		g_assert_not_reached ();
+	egg_asn1x_destroy (asn);
+	g_free (data);
+
+	data = testing_data_read ("test-pkcs8-1.der", &n_data);
+	asn = egg_asn1x_create (pkix_asn1_tab, "pkcs-8-PrivateKeyInfo");
+	egg_asn1x_dump (asn);
+	if (!egg_asn1x_decode (asn, data, n_data))
+		g_assert_not_reached ();
+	egg_asn1x_destroy (asn);
+	g_free (data);
 
 	data = testing_data_read ("test-certificate-1.der", &n_data);
 	asn = egg_asn1x_create (pkix_asn1_tab, "Certificate");
