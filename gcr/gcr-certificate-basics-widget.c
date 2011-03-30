9eb4d9d104c695e2b29005fd953d2c5138c0d75e
Stef Walter <stefw@collabora.co.uk>
2011-03-30 15:06:07 +0200

    gcr: Implement gcr_certificate_basics_xxx() stubs properly.

--- gcr/gcr-certificate-basics-widget.c
+++ gcr/gcr-certificate-basics-widget.c
@@ -20,35 +20,35 @@
  */
 
 #include "config.h"
-#include "gcr-certificate-details-widget.h"
+#include "gcr-certificate-basics-widget.h"
 
 #ifndef GCR_DISABLE_DEPRECATED
 
 /* These are all stubs for GcrCertificateWidget */
 
 GType
-gcr_certificate_details_widget_get_type (void)
+gcr_certificate_basics_widget_get_type (void)
 {
 	return gcr_certificate_widget_get_type ();
 }
 
-GcrCertificateDetailsWidget*
-gcr_certificate_details_widget_new (GcrCertificate *cert)
+GcrCertificateBasicsWidget*
+gcr_certificate_basics_widget_new (GcrCertificate *cert)
 {
 	return gcr_certificate_widget_new (cert);
 }
 
 GcrCertificate*
-gcr_certificate_details_widget_get_certificate (GcrCertificateDetailsWidget *details)
+gcr_certificate_basics_widget_get_certificate (GcrCertificateBasicsWidget *basics)
 {
-	return gcr_certificate_widget_get_certificate (details);
+	return gcr_certificate_widget_get_certificate (basics);
 }
 
 void
-gcr_certificate_details_widget_set_certificate (GcrCertificateDetailsWidget *details,
+gcr_certificate_basics_widget_set_certificate (GcrCertificateBasicsWidget *basics,
                                                 GcrCertificate *cert)
 {
-	return gcr_certificate_widget_set_certificate (details, cert);
+	return gcr_certificate_widget_set_certificate (basics, cert);
 }
 
 #endif /* GCR_DISABLE_DEPRECATED */
