GNOME/gcr

gck: Fix precondition in gck_object_from_handles

gck/gck-object.c
@@ -242,7 +242,7 @@ gck_objects_from_handle_array (GckSession *session,
 	CK_ULONG i;
 
 	g_return_val_if_fail (GCK_IS_SESSION (session), NULL);
-	g_return_val_if_fail (object_handles == NULL || n_object_handles == 0, NULL);
+	g_return_val_if_fail (n_object_handles == 0 || object_handles != NULL, NULL);
 
 	for (i = 0; i < n_object_handles; ++i)
 		results = g_list_prepend (results, gck_object_from_handle (session, object_handles[i]));

GNOME/gnome-keyring

gck/gck-object.c
@@ -233,7 +233,7 @@ gck_objects_from_handle_array (GckSession *session, CK_OBJECT_HANDLE_PTR handles
 	CK_ULONG i;
 
 	g_return_val_if_fail (GCK_IS_SESSION (session), NULL);
-	g_return_val_if_fail (handles || !n_handles, NULL);
+	g_return_val_if_fail (n_handles == 0 || handles != NULL, NULL);
 
 	for (i = 0; i < n_handles; ++i)
 		results = g_list_prepend (results, gck_object_from_handle (session, handles[i]));