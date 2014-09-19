f094c832805dac7cc4456b431c522b34258dd844
tomsod-m@ya.ru <tomsod-m@ya.ru>
2014-09-19 13:06:00 +0000

    tool: Remove residual GTK+ usage
    https://bugzilla.gnome.org/show_bug.cgi?id=736973

--- tool/gkr-tool.c
+++ tool/gkr-tool.c
@@ -25,7 +25,6 @@
 #include "gkr-tool.h"
 
 #include <glib/gi18n.h>
-#include <gtk/gtk.h>
 
 #include <locale.h>
 #include <string.h>
@@ -129,8 +128,6 @@ main (int argc, char *argv[])
 	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
 #endif
 
-	gtk_init (&argc, &argv);
-	
 	/* The first argument is the command */
 	if (argc < 2) {
 		print_general_usage ();
