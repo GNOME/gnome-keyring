760f60931a13c6c422a09dca47eb8cb69898d0ec
Milan Crha <mcrha@redhat.com>
2015-08-06 12:04:00 +0000

    Fix issue found by address sanitizer
    There was done a read of one item more than the array was allocated.
    This patch fixes the issue.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=752919

--- pkcs11/rpc-layer/gkm-rpc-util.c
+++ pkcs11/rpc-layer/gkm-rpc-util.c
@@ -81,9 +81,10 @@ gkm_rpc_mechanism_list_purge (CK_MECHANISM_TYPE_PTR mechs, CK_ULONG* n_mechs)
 	for (i = 0; i < (int)(*n_mechs); ++i) {
 		if (!gkm_rpc_mechanism_has_no_parameters (mechs[i]) &&
 		    !gkm_rpc_mechanism_has_sane_parameters (mechs[i])) {
-
-			/* Remove the mechanism from the list */
-			memmove (&mechs[i], &mechs[i + 1], (*n_mechs - i) * sizeof (CK_MECHANISM_TYPE));
+			if (*n_mechs - i - 1 > 0) {
+				/* Remove the mechanism from the list */
+				memmove (&mechs[i], &mechs[i + 1], (*n_mechs - i - 1) * sizeof (CK_MECHANISM_TYPE));
+			}
 
 			--(*n_mechs);
 			--i;
