ad1d71aa5c6d941a1319bb78bfce797562781572
Jakub Jelen <jjelen@redhat.com>
2017-08-08 19:08:58 +0200

    Test that ssh-agent started and sets environment

--- daemon/test-startup.c
+++ daemon/test-startup.c
@@ -245,6 +245,27 @@ test_daemon_replace (Test *test,
 	g_assert_cmpint (status, ==, 0);
 }
 
+static void
+test_ssh_agent (Test *test,
+                        gconstpointer unused)
+{
+	gchar *auth_sock = g_build_filename (test->directory, "keyring", "ssh", NULL);
+
+	const gchar *argv[] = {
+		BUILDDIR "/gnome-keyring-daemon", "--foreground",
+		"--components=ssh-agent", NULL
+	};
+
+	gchar **output;
+
+	output = gkd_test_launch_daemon (test->directory, argv, &test->pid, NULL);
+	g_assert_cmpstr (g_environ_getenv (output, "SSH_AUTH_SOCK"), ==, auth_sock);
+	g_strfreev (output);
+
+	g_assert (g_file_test (auth_sock, G_FILE_TEST_EXISTS));
+	g_free (auth_sock);
+}
+
 int
 main (int argc, char **argv)
 {
@@ -263,6 +284,8 @@ main (int argc, char **argv)
 
 	g_test_add ("/daemon/startup/replace", Test, NULL,
 	            setup, test_daemon_replace, teardown);
+	g_test_add ("/daemon/startup/ssh-agent", Test, NULL,
+	            setup, test_ssh_agent, teardown);
 
 	return g_test_run ();
 }
